4db0c78c115f72903f23a5752b1dcf8e4583e31b
Djuliadark <71487396+Djuliadark@users.noreply.github.com>
2020-10-15 23:37:30 +0300

    Add files via upload

---
+++ Work1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,b,S,P;
+printf("Ввод a:");
+scanf("%d",&a);
+printf("Ввод b:");
+scanf("%d",&b);
+S = a * b;
+P = 2 * (a + b);
+printf (" S = %d.\n",S);
+printf (" P = %d.\n",P);
+return 0; 
+
+}
